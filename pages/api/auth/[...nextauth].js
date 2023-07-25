5d2dc2f439c6b0b3b1d13acfbd53dc90ec0afd42
mohammedsaif-openturf <mohammed.saif@openturf.in>
2023-07-25 22:12:33 +0530

    Login Page & Integrating API

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,10 +2,22 @@ import NextAuth from 'next-auth';
 import SpotifyProvider from 'next-auth/providers/spotify';
 import SpotifyAPI, { LOGIN_URL } from '../../../lib/spotify';
 
+
+// refer Next auth token rotation page for detail explanation
+// https://next-auth.js.org/v3/tutorials/refresh-token-rotation
+
+
+/**
+ * Takes a token, and returns a new token with updated
+ * `accessToken` and `accessTokenExpires`. If an error occurs,
+ * returns the old token and an error property
+ */
 async function refreshAccessToken(token) {
     try {
         SpotifyAPI.setAccessToken(token.accessToken);
         SpotifyAPI.setAccessToken(token.refreshToken);
+
+        // After sending the above 2 tokens - access token and refresh token, we are getting the new access token ; as the refresh token doesn't expire only the access token does
 
         const { body: refreshedToken } = await SpotifyAPI.refreshAccessToken();
 
@@ -14,9 +26,9 @@ async function refreshAccessToken(token) {
         return {
             ...token,
             accessToken: refreshedToken.access_token,
-            accessTokenExpires: Date.now() + refreshToken.expires_in * 1000,
-            refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
-        };
+            accessTokenExpires: Date.now() + refreshToken.expires_in * 1000, //1hr as 3600 returns from spotify API
+            refreshToken: refreshedToken.refresh_token ?? token.refreshToken,// replace if the new one came back else fall back to old refresh token
+        }
     } catch (error) {
         console.error(error);
 
@@ -28,11 +40,13 @@ async function refreshAccessToken(token) {
 }
 
 export default NextAuth({
+    // Configure one or more authentication providers
+
     providers: [
         SpotifyProvider({
             clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
             clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
-            authorization: LOGIN_URL,
+            authorization: LOGIN_URL, //User will be sent to authorization page from SPotify
         }),
     ],
     secret: process.env.JWT_SECRET,
@@ -40,26 +54,29 @@ export default NextAuth({
         signIn: '/login',
     },
     callbacks: {
+        // Getting JWT token after signing in from spotify
+
         async jwt({ token, account, user }) {
             // Initil Sign In
             if (account && user) {
+                console.log("Initial Sigin", account)
                 return {
                     ...token,
                     accessToken: account.access_token,
                     refreshToken: account.refresh_token,
                     username: account.providerAccountId,
-                    accessTokenExpires: account.expires_at * 1000,
+                    accessTokenExpires: account.expires_at * 1000,//we are handling expiry times in ms hence * 1000
                 };
             }
 
             // Return previous token if the access token hasn't expired yet
             if (Date.now() < token.accessTokenExpires) {
-                console.log('Token is valid ...');
+                console.log("Existing Access Token Is Valid")
                 return token;
             }
 
             // Access Token expired, so refresh it...token
-            console.log('Token expired, refreshing...');
+            console.log("Access Token Has Expired, Refreshing")
             return await refreshAccessToken(token);
         },
 
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,8 +1,30 @@
-const LoginPage = () => {
+import { getProviders, signIn } from "next-auth/react"
+
+const LoginPage = ({ providers }) => {
+    const providersArray = Object.values(providers);
+
     return (
-        <div>
-            <h1>This is a Login Page</h1>
+        <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+            <img src="https://links.papareact.com/9xl" alt="spotify_logo" className="w-52 mb-5" />
+            {providersArray.map((provider, index) => (
+                <div key={provider.name}>
+                    <button className="bg-[#18D860] rounded-full text-white p-5"
+                        onClick={() => signIn(provider.id, {
+                            callbackUrl: "/"
+                        })}
+                    >Login with {provider.name}</button>
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
+
+export const getServerSideProps = async () => {
+    const providers = await getProviders();
+    return {
+        props: { providers },
+    };
+};
+
+
 export default LoginPage
