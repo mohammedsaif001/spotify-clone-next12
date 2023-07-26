8ad604dc5746d9ee6737c555e3b8dec95ab36ba2
mohammedsaif-openturf <mohammed.saif@openturf.in>
2023-07-26 16:47:33 +0530

    Song Atom and Playing from external Spotify Connection

--- components/Song.js
+++ components/Song.js
@@ -1,16 +1,36 @@
-
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSpotify from "../hooks/useSpotify";
 import { millisToMinutesAndSeconds } from "../lib/time";
+import { useRecoilState } from "recoil";
 
 const Song = ({ order, track }) => {
-    const spotifyApi = useSpotify()
+    const spotifyApi = useSpotify();
+    const [currentTrackId, setCurrentTrackId] =
+        useRecoilState(currentTrackIdState);
+    const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+    const playSong = () => {
+        setCurrentTrackId(track?.track.id);
+        setIsPlaying(true);
+        spotifyApi.play({
+            uris: [track.track.uri]
+        })
+    };
+
     return (
         <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
-            <div className="flex items-center space-x-4">
+            <div className="flex items-center space-x-4" onClick={playSong}>
                 <p>{order + 1}</p>
-                <img src={track.track.album.images[0].url} alt={"Image Song"} loading="lazy" className="h-10 w-10" />
-                <div >
-                    <p className="w-36 lg:w-64 text-white truncate font-semibold">{track?.track.name}</p>
+                <img
+                    src={track.track.album.images[0].url}
+                    alt={"Image Song"}
+                    loading="lazy"
+                    className="h-10 w-10"
+                />
+                <div>
+                    <p className="w-36 lg:w-64 text-white truncate font-semibold">
+                        {track?.track.name}
+                    </p>
                     <p>{track?.track.artists[0].name}</p>
                 </div>
             </div>
@@ -20,6 +40,6 @@ const Song = ({ order, track }) => {
                 <p>{millisToMinutesAndSeconds(track?.track.duration_ms)}</p>
             </div>
         </div>
-    )
-}
-export default Song
+    );
+};
+export default Song;
