a6c4610166ae0fa729807098db2a5aaaad152f6c
mohammedsaif-openturf <mohammed.saif@openturf.in>
2023-07-26 18:54:56 +0530

    useSong Hook

--- components/Player.js
+++ components/Player.js
@@ -3,6 +3,7 @@ import useSpotify from "../hooks/useSpotify"
 import { useRecoilState } from "recoil"
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom"
 import { useState } from "react"
+import useSongInfo from "../hooks/useSongInfo"
 
 const Player = () => {
     const sptifyApi = useSpotify()
@@ -10,6 +11,8 @@ const Player = () => {
     const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState)
     const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState)
     const [volume, setVolume] = useState(50)
+
+    const songInfo = useSongInfo()
 
     return (
         <div>
--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -4,6 +4,7 @@ import SpotifyWebApi from "spotify-web-api-node";
 
 const spotifyAPI = new SpotifyWebApi({
     clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+    clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
 });
 
 const useSpotify = () => {
