5cb1cb7ebfa68992e59a8a62c0ece164886d5cf8
mohammedsaif-openturf <mohammed.saif@openturf.in>
2023-07-26 15:01:32 +0530

    Creating a custom hook for spotifyapi

---
+++ hooks/useSpotify.js
@@ -0,0 +1,25 @@
+import { useSession, signIn } from "next-auth/react";
+import { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyAPI = new SpotifyWebApi({
+    clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+});
+
+const useSpotify = () => {
+    const { data: session } = useSession();
+
+    useEffect(() => {
+        if (session) {
+            // While refreshing token if we are getting below error defined in [...nextauth].js (Error Catching) throw them to login page as we are not able to refresh the token
+            if (session.error === "RefreshAccessTokenError") {
+                signIn()
+            }
+
+            spotifyAPI.setAccessToken(session?.user?.accessToken)
+        }
+    }, [session])
+
+    return spotifyAPI;
+}
+export default useSpotify
