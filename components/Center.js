136507003af5cab2618a64093d659c05c1273e4d
mohammedsaif-openturf <mohammed.saif@openturf.in>
2023-07-26 16:00:25 +0530

    Fetching Songs from playlist and sidebar responsiveness

--- components/Center.js
+++ components/Center.js
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react";
 import { shuffle } from "lodash"
 import { useRecoilState, useRecoilValue } from "recoil";
 import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
+import Songs from "./Songs";
 
 const colors = [
     "from-indigo-500",
@@ -18,6 +20,8 @@ const colors = [
 const Center = () => {
     const { data: session } = useSession();
     const [color, setColor] = useState(null)
+    const spotifyApi = useSpotify()
+
     // const [playlistID, setPlaylistID] = useRecoilState(playlistIdState) If you want only readonly access then use the below import
     const playlistID = useRecoilValue(playlistIdState)
     const [playlist, setPlaylist] = useRecoilState(playlistState)
@@ -27,10 +31,20 @@ const Center = () => {
         setColor(shuffle(colors).pop())
     }, [playlistID])
 
+    useEffect(() => {
+
+        // getting songs from playlist
+        spotifyApi.getPlaylist(playlistID).then((data) => {
+            setPlaylist(data?.body)
+        }).catch((err) => {
+            console.log("Something Went Wrong", err.message)
+        })
+    }, [spotifyApi, playlistID])
+    console.log(playlist)
     return (
         <div className="flex-grow">
             <header className="absolute top-5 right-8">
-                <div className="flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
+                <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white">
                     <img
                         src={session?.user?.image}
                         alt="spotify_image"
@@ -41,8 +55,16 @@ const Center = () => {
                 </div>
             </header>
             <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
-                {/* <img src="" alt="" /> */}
+                <img src={playlist?.images[0]?.url || `https://styles.redditmedia.com/t5_2qofj/styles/communityIcon_2y6i62hipc731.png`} alt="Playlist_Image" className="h-44 w-44 shadow-2xl" />
+                <div>
+                    <p>PLAYLIST</p>
+                    <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">{playlist?.name}</h1>
+                </div>
             </section>
+
+            <div>
+                <Songs />
+            </div>
         </div>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 import Center from '../components/Center'
+import { getSession } from 'next-auth/react';
 
 export default function Home() {
   return (
@@ -20,4 +21,15 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+
+// Fetching Session pre-hand in the server otself
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session
+    }
+  }
 }
