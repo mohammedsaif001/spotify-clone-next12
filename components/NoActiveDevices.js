08338c999f4ad5a1b0a27dc4cb6e202eccf926ea
mohammedsaif-openturf <mohammed.saif@openturf.in>
2023-07-27 16:19:21 +0530

    UI Fix

--- components/NoActiveDevices.js
+++ components/NoActiveDevices.js
@@ -1,9 +1,9 @@
 const NoActiveDevices = ({ msg, instruction }) => {
-    return <div className='text-white'>
-        <h2>{msg}</h2>
-        <p>{instruction}</p>
+    return <div className='text-white flex flex-col justify-center items-center w-full h-full'>
+        <h2 className="text-lg tracking-wider uppercase">{msg}</h2>
+        <p className="text-lg mb-5 tracking-wider font-semibold uppercase">{instruction}</p>
         <img src="https://links.papareact.com/9xl" alt="spotify_logo" className="w-52 mb-5" />
-        <button onClick={() => window.location.reload()} className='bg-green-800 p-3 rounded-xl text-lg font-semibold hover:bg-green-900'>Refresh Page</button>
+        <button onClick={() => window.location.reload()} className='bg-green-800 p-3 rounded-xl text-lg font-semibold hover:bg-green-900 tracking-wide'>Refresh Page</button>
     </div>
 }
 
